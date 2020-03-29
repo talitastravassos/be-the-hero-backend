31bf99a7f67d900aa46f379ef6e771784f9c952b
talitastravassos <talitatravassos.tt@gmail.com>
2020-03-29 14:01:26 -0300

    project structure

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes')
+
+app.use(express.json())
+app.use(routes)
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, function () {
+  console.log(`Node.js listening on port ${port}...`);
+});
