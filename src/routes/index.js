27718b27023868eb2a0310371b162d671c6c1bf0
talitastravassos <talitatravassos.tt@gmail.com>
2020-03-29 15:50:29 -0300

    add: post and get ongs routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,40 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('../database/connection');
 
 const routes = express.Router();
 
 routes.get('/', function (request, response) {
   response.send('Hello');
+});
+
+routes.get('/ongs', async function (request, response) {
+  const ongs = await connection('ongs').select('*');
+
+  response.json(ongs);
+});
+
+routes.post('/ongs', async function (request, response) {
+  const { name, email, whatsapp, city, uf } = request.body;
+
+  console.log(request.body);
+
+  const id = crypto.randomBytes(5).toString('HEX');
+
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+  response.json({
+    status: response.statusCode,
+    responseText: 'success',
+    id
+  });
 });
 
 module.exports = routes;
